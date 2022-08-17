78719d6dad644bfa3a3fed3d10405df390821ef1
Aathi Vignesh <aathivigesh221@gmail.com>
2022-08-17 20:40:16 +0530

    changed everything into two models using devise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
-  before_action :authenticate_userpage!
   # GET /tasks or /tasks.json
   def index
     @tasks = Task.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
-  before_action :authenticate_userpage!
- 
   # GET /users or /users.json
   def index
     @users = User.all
